5e8ccb6ae428749ea622c7d6747e60e30f8a49a4
Raymond Chen <ahrchen@gmail.com>
2022-04-02 22:13:29 -0700

    Initial Commit

---
+++ MeetUpable/MeetUpableApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetUpableApp.swift
+//  MeetUpable
+//
+//  Created by Raymond Chen on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MeetUpableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
